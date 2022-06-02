f98efccf53ecdffd4e4c6e05cd40945d2fcda19e
jykim <empireofwind@gmail.com>
2022-06-02 13:57:57 +0900

    DOM을 이용한 HTML 객체 저장

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,3 +1,18 @@
 "use strict"
 
-console.log("hello");
+// DOM : Document Object Model
+const id = document.querySelector("#id"),
+     pwd = document.querySelector("#pwd"),
+     loginBtn = document.querySelector("button");
+
+console.log(id);
+
+loginBtn.addEventListener("click", login);
+
+
+function login(){
+    const req = {
+        id: id.value,
+        pwd: pwd.value,
+    };
+}
