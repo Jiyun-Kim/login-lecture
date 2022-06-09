46f0abaa2ea94da706913383c5cc366e7f6506c7
jykim <empireofwind@gmail.com>
2022-06-09 15:45:21 +0900

    fs모듈을 통한 회원가입 기능 구현

--- app/src/routes/home/index.js
+++ app/src/routes/home/index.js
@@ -8,6 +8,7 @@ const ctrl = require("./home.ctrl"); // 컨트롤러에서 불러옴
 router.get("/", ctrl.output.home);
 router.get("/login", ctrl.output.login);
 router.get("/register", ctrl.output.register);
+
 router.post("/login", ctrl.process.login);
 router.post("/register", ctrl.process.register);
 
