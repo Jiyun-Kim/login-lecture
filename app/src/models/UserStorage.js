439becd329d5471bd8b36766858a4fc714b13404
jykim <empireofwind@gmail.com>
2022-06-10 17:04:32 +0900

    백엔드 맛보기 최종 최적화

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -12,7 +12,7 @@ class UserStorage {
 
     static getUserInfo(id) {
         return new Promise((resolve, reject) => {
-            const query = "SELECT id, pwd FROM users where id = $1;";
+            const query = "SELECT id, pwd FROM adfs where id = $1;";
             db.query(query, [id], (err, data) => {
                 if (err) reject(`${err}`);
                 else resolve(data.rows[0]);
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -8,10 +8,12 @@ const output = {
         logger.info(`GET / 200 "홈 화면으로 이동"`);
         res.render("home/index");
     },
+
     login: (req, res) => {
         logger.info(`GET / login 200 "로그인 화면으로 이동"`);
         res.render("home/login");
     },
+    
     register: (req, res) => {
         logger.info(`GET / register 200 "회원가입 화면으로 이동"`);
         res.render("home/register");
@@ -22,20 +24,25 @@ const process = {
     login: async (req, res) => {
         const user = new User(req.body);
         const response = await user.login();
-        if (response.err)
-            logger.error(`POST / login 200 Response: "success: ${ response.success }, ${response.err}"`);
-        else
-            logger.info(`POST / login 200 Response: "success: ${ response.success }, msg: ${response.msg}"`);
-        return res.json(response);
+        const url = {
+            method: "POST",
+            path: "/login",
+            status: response.err ? 400 : 200,
+        }
+        log(response, url);
+        return res.status(url.status).json(response);
     },
+
     register: async (req, res) => {
         const user = new User(req.body);
         const response = await user.register();
-        if (response.err)
-            logger.error(`POST / login 200 Response: "success: ${ response.success }, ${response.err}"`);
-        else
-            logger.info(`POST / login 200 Response: "success: ${ response.success }, msg: ${response.msg}"`);
-        return res.json(response);
+        const url = {
+            method: "POST",
+            path: "/register",
+            status: response.err ? 409 : 201,
+        }
+        log(response, url);
+        return res.status(url.status).json(response);
     },
 };
 
@@ -46,3 +53,16 @@ module.exports = { // 다른곳에서 사용할 수 있도록 object로 빼줌
 
 // object 는 원래 {key : value} 구조인데 key 만 입력하면 자체적으로 key와 같은 value를 넣어주게 됨
 // ex) hello, login 이런식으로 입력 시 hello : hello, login : login 과 같은 의미
+
+const log = (response, url) => {
+    if (response.err) {
+        logger.error(
+            `${url.method} ${url.path} ${url.status} Response: ${ response.success }, ${response.err}`
+        );
+    }
+    else {
+        logger.info(
+            `${url.method} ${url.path} ${url.status} Response: ${ response.success }, ${response.msg || ""}`
+        );
+    }
+}
